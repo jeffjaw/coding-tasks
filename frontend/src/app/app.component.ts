a4830cf13317e1ec72bf6b82a96f098a0aa68156
Jeff <jeffj0619@gmail.com>
2022-09-07 22:07:41 +0800

    complete frontend task

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './interfaces/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'front-end-task';
+export class AppComponent implements OnInit {
+  users_raw: User[] = [];
+  users: User[] = [];
+  searchName = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  onSearchChanges(event: any) {
+    this.users = this.users_raw.filter((user) => {
+      return user.name.toLowerCase().includes(this.searchName.toLowerCase());
+    });
+  }
+
+  onResetBalance(event: any) {
+    this.users = this.users_raw
+      .map((user) => {
+        user.balanceNum = 0;
+        return user;
+      })
+      .filter((user) => {
+        return user.name.toLowerCase().includes(this.searchName.toLowerCase());
+      });
+  }
+
+  loadUsers() {
+    return this.http.get<User[]>('/assets/users.json').subscribe((users) => {
+      users.sort((a, b) => a.name.localeCompare(b.name));
+      this.users_raw = users.map((user) => {
+        user.registered = user.registered.replace(/ /g, '');
+        user.balanceNum = parseFloat(user.balance.replace(/[^0-9\.]+/g, ''));
+        return user;
+      });
+      this.users = this.users_raw;
+    });
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule, TableModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
