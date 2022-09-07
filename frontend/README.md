cdd2675654748126f90037b4ccbf4d750594f7bb
Jeff <jeffj0619@gmail.com>
2022-09-07 23:25:33 +0800

    update readme.md

--- frontend/README.md
+++ frontend/README.md
@@ -1,27 +1,10 @@
-# FrontEndTask
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.1.
+## User Story
+As the System User I would like to see active user’s personal data in a table so that I may make management decisions. I would like to filter, order and search the data to help me make those decisions.  
 
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
 ## Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { User } from './interfaces/user';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  users_raw: User[] = [];
+  rawUsersData: User[] = [];
   users: User[] = [];
   searchName = '';
 
@@ -19,13 +19,14 @@ export class AppComponent implements OnInit {
   }
 
   onSearchChanges(event: any) {
-    this.users = this.users_raw.filter((user) => {
+    this.users = this.rawUsersData.filter((user) => {
       return user.name.toLowerCase().includes(this.searchName.toLowerCase());
     });
   }
 
   onResetBalance(event: any) {
-    this.users = this.users_raw
+    var test = '';
+    this.users = this.rawUsersData
       .map((user) => {
         user.balanceNum = 0;
         return user;
@@ -38,12 +39,16 @@ export class AppComponent implements OnInit {
   loadUsers() {
     return this.http.get<User[]>('/assets/users.json').subscribe((users) => {
       users.sort((a, b) => a.name.localeCompare(b.name));
-      this.users_raw = users.map((user) => {
-        user.registered = user.registered.replace(/ /g, '');
-        user.balanceNum = parseFloat(user.balance.replace(/[^0-9\.]+/g, ''));
-        return user;
-      });
-      this.users = this.users_raw;
+      this.rawUsersData = users
+        .map((user) => {
+          user.registered = user.registered.replace(/ /g, '');
+          user.balanceNum = parseFloat(user.balance.replace(/[^0-9\.]+/g, ''));
+          return user;
+        })
+        .filter((user) => {
+          return user.isActive;
+        });
+      this.users = this.rawUsersData;
     });
   }
 }
